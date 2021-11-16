b7b0d17e7378770c43459ba1d5bf56eb62cb2e53
HVEVB <36706634+VBDojnaz@users.noreply.github.com>
2021-11-16 04:53:04 +0100

    Working script

--- cloudflared/start.sh
+++ cloudflared/start.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-CONFIG=/home/container
-if test -f "$CONFIG"; then
-	echo "exists"
+CERT=/home/container/.cloudflared/cert.pem
+if test -f "$CERT"; then
+	cd .cloudflared
+    TUNNELS=`ls -1 *.json 2>/dev/null | wc -l`
+    if [ $TUNNELS != 0 ]; then
+        cloudflared tunnel run
+    else
+    	NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+        cloudflared tunnel create dcloud$NEW_UUID
+        TUNNEL=$(cut -c 3-38 <<< $(find . -type f -name "*.json"))
+        curl -o /home/container/.cloudflared/config.yml https://raw.githubusercontent.com/Dojnaz/DojnazCloudEggs/main/cloudflared/config.yml
+        sed -i "s/TUNNEL_ID/$TUNNEL/" /home/container/.cloudflared/config.yml
+    fi
 else
-	echo "doesn't exist"
+    cloudflared tunnel login
 fi
