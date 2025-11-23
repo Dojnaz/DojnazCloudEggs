8621640d4b0b58e96bf7ba62d058c379afb9ad6b
Vee Bäck <36706634+VeeBack@users.noreply.github.com>
2025-11-23 02:02:09 +0100

    Specify full path for cloudflared commands

--- cloudflared/start.sh
+++ cloudflared/start.sh
@@ -5,14 +5,14 @@ if test -f "$CERT"; then
 	cd .cloudflared
     TUNNELS=`ls -1 *.json 2>/dev/null | wc -l`
     if [ $TUNNELS != 0 ]; then
-        cloudflared tunnel run
+        /usr/bin/cloudflared tunnel run
     else
     	NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-        cloudflared tunnel create dcloud$NEW_UUID
+        /usr/bin/cloudflared tunnel create dcloud$NEW_UUID
         TUNNEL=$(cut -c 3-38 <<< $(find . -type f -name "*.json"))
         curl -o /home/container/.cloudflared/config.yml https://raw.githubusercontent.com/Dojnaz/DojnazCloudEggs/main/cloudflared/config.yml
         sed -i "s/TUNNEL_ID/$TUNNEL/" /home/container/.cloudflared/config.yml
     fi
 else
-    cloudflared tunnel login
+    /usr/bin/cloudflared tunnel login
 fi
