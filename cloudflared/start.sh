3e3f8b875354fd9771daee85222efcba8b9234b8
Vee Bäck <36706634+VeeBack@users.noreply.github.com>
2025-11-23 02:04:04 +0100

    Update cloudflared commands to use relative paths

--- cloudflared/start.sh
+++ cloudflared/start.sh
@@ -5,14 +5,14 @@ if test -f "$CERT"; then
 	cd .cloudflared
     TUNNELS=`ls -1 *.json 2>/dev/null | wc -l`
     if [ $TUNNELS != 0 ]; then
-        /usr/bin/cloudflared tunnel run
+        ../cloudflared tunnel run
     else
     	NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-        /usr/bin/cloudflared tunnel create dcloud$NEW_UUID
+        ../cloudflared tunnel create dcloud$NEW_UUID
         TUNNEL=$(cut -c 3-38 <<< $(find . -type f -name "*.json"))
         curl -o /home/container/.cloudflared/config.yml https://raw.githubusercontent.com/Dojnaz/DojnazCloudEggs/main/cloudflared/config.yml
         sed -i "s/TUNNEL_ID/$TUNNEL/" /home/container/.cloudflared/config.yml
     fi
 else
-    /usr/bin/cloudflared tunnel login
+    ./cloudflared tunnel login
 fi
